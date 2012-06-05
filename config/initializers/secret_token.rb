f83b8c5ed69505175b1b0f0d6e876d3adaeb4a19
Paweł Usowski <pawel.usowski@gmail.com>
2012-06-05 20:52:58 +0200

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Terminarz::Application.config.secret_token = '2dcdac4e3aac764cd1435af2b05f4645b599452aa6ae112f38170178db8c01101ac8805a560d15e753a27b4fbd50172757bd75849c264c3b228f961d3de04f93'
