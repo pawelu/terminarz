2646fdf8337e2735e0be71f6f384cddda1a50857
Paweł Usowski <pawel.usowski@gmail.com>
2012-07-20 22:57:35 +0200

    podrasowany wygląd - progress bary itp

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,16 @@
 module TasksHelper
+	def date_diff(date)
+		(date - Date.today).to_i
+	end
+
+	def progress_class(value)
+		color_class = case value
+		when 0...15
+			"progress-danger"
+		when 10...33
+			"progress-warning"
+		else
+			"progress-success"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Terminarz::Application.routes.draw do
+  root to: 'tasks#index'
   resources :tasks
 
   # The priority is based upon order of creation:
