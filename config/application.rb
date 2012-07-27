7a58ee9203aa20b849dc8334fb02e99b641c31c2
Paweł Usowski <pawel.usowski@gmail.com>
2012-07-27 22:44:16 +0200

    pare drobnych poprawek [PL]

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ end
 
 module Terminarz
   class Application < Rails::Application
+    I18n.default_locale = :pl
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
